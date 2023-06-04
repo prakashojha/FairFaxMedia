ee4eeb654212c3522650d2c2d17e7968cfe5b361
prakashojha2005@gmail.com <prakashojha2005@gmail.com>
2023-06-04 17:27:02 +1200

    use case added in domain layer

--- FairFaxMedia/Domain/UseCase/ArticleUseCases.swift
+++ FairFaxMedia/Domain/UseCase/ArticleUseCases.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class ArticleUseCases{
+    
+    let articleDataService: ArticleDataRepo
+    
+    public init(articleDataService: ArticleDataRepo){
+        self.articleDataService = articleDataService
+    }
+    
+    func executeUseCaseGetArticleData() async -> Result<[ArticleEntity], Error>{
+        let result: Result<[ArticleEntity], Error>  = await articleDataService.getArticleData()
+        switch(result){
+        case .success(let articleEntities):
+            return .success(articleEntities)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    func executeUseCaseGetArticleImage(from url: String) async -> Result<Data?, Error>{
+        let result: Result<Data?, Error> = await articleDataService.getArticleImage(from: url)
+        switch(result){
+        case .success(let imageData):
+            return .success(imageData)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+}
