43b70684bbc58ebea6aed4820ff4bfd6c5b3e9b2
prakashojha2005@gmail.com <prakashojha2005@gmail.com>
2023-06-04 16:44:51 +1200

    domain layer created

---
+++ FairFaxMedia/Domain/Entity/ArticleEntity.swift
@@ -0,0 +1,32 @@
+//
+//  ArticleEntity.swift
+//  FairFaxMedia
+//
+//  Created by bindu.ojha on 4/06/23.
+//
+
+import Foundation
+
+struct ArticleImage{
+    var url: String?
+    var width: Int?
+    var height: Int?
+    var type: String?
+    
+    init(url: String? = nil, width: Int? = nil, height: Int? = nil, type: String? = nil) {
+        self.url = url
+        self.width = width
+        self.height = height
+        self.type = type
+    }
+}
+
+struct ArticleEntity{
+    var headline: String?
+    var abstract: String?
+    var author: String?
+    var articleImages: [ArticleImage]?
+    var timeStamp: Int?
+    
+    
+}
